c32a8af684c060581fed1be6ea794a0b988faec9
shaoxiaodan <sandays_cn@163.com>
2024-08-07 12:14:33 -0700

    shiro的基础测试方法

---
+++ src/test/java/com/example/demo/ShiroTest.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShiroTest {
+
+	// Shiro（自带默认）最简单的实现Realm
+	private SimpleAccountRealm accountRealm = new SimpleAccountRealm();
+
+	// Shiro的安全认证管理器
+	private DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+
+	/*
+	 * 初始化数据
+	 */
+	public void init() {
+		System.out.println("init ...");
+
+		// 注册用户信息
+		accountRealm.addAccount("aaa", "111");
+		accountRealm.addAccount("bbb", "222");
+		accountRealm.addAccount("ccc", "333");
+
+		// 构建认证环境
+		defaultSecurityManager.setRealm(accountRealm);
+	}
+
+	@Test
+	public void testAuthentication() {
+		System.out.println("testAuthentication ...");
+		
+		// 数据初始化
+		init();
+		
+		// 环境初始化，设置上下文
+		SecurityUtils.setSecurityManager(defaultSecurityManager);
+		
+		// 当前操作主题
+		Subject subject = SecurityUtils.getSubject();
+		
+		//构造(外部)的登录操作，提供用户名字+密码
+		UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("aaa", "111");
+		
+		// 登录认证操作
+		subject.login(usernamePasswordToken);
+		
+		// 判断认证结果
+		System.out.println("认证结果=" + subject.isAuthenticated());
+	}
+
+}
